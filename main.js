6735905229b5ba866d3fb5e834d6747ae8207916
Dawitlior <dawitlior777@gmail.com>
2022-06-27 13:00:34 +0300

    add

--- main.js
+++ main.js
@@ -1,51 +1,64 @@
-async function getTheName() {
+const BASIC_API = "https://moviesmern.herokuapp.com/movies/";
+async function getTheMovie() {
   try {
-    return await fetch("https://moviesmern.herokuapp.com/movies/all").then(
-      (response) => {
-        return response.json();
-      }
-    );
+    return await fetch(
+      `${BASIC_API}movie/searchByName/${searchMovies.value}`
+    ).then((response) => response.json());
   } catch (error1) {
-    alert("some error");
+    alert(error1);
   } finally {
   }
 }
 
+async function sendTheMovie() {
+  const data = {
+    movie: {
+      image: input.value,
+      linkToMovie: input1.value,
+      movieName: input2.value,
+      rating: input3.value,
+      synopsis: input4.value,
+    },
+  };
+  try {
+    await fetch("https://moviesmern.herokuapp.com/movies/saveMovie", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "content-type": "application/json" },
+    });
+  } catch (error) {
+    alert("sorry about the error");
+  } finally {
+  }
+}
 
 function printNamesToScreen() {
-  getTheName()
+  getTheMovie()
     .then((result) => {
       result.data.forEach((item) => {
-        my_ul.innerHTML += `<li>${item.movieName}</li>`+`<img src='${item.image}'/>`
+        my_ul.innerHTML =
+          `<li>${item.movieName}</li>` + `<img src='${item.image}'/>`;
       });
     })
     .catch(() => {})
     .finally(() => {});
 }
 
+// async function getMovieByName() {
+//   try {
+//     return await fetch(
+//       `${BASIC_API}movie/searchByName/${searchMovies.value}`
+//     ).then((res) => res.json);
+//   } catch (err) {
+//     console.log(err);
+//   } finally {
+//   }
+// }
+// function showMovieByName(){
 
-
-
-const data = {
-  
-  image:"https://d3m9l0v76dty0.cloudfront.net/system/photos/2556967/large/9f26b1108becb59ae5e27cdfc0c9b55f.jpg",
-  linkToMovie: "https://www.icerbox.biz/watch/C3FA6C15B0E23A1",
-  movieName: "LiorDawit",
-  rating: 8,
-  synopsis: "oh snap",
-};
-async function postTheMovie() {
-  try {
-    return await fetch("https://moviesmern.herokuapp.com/movies/saveMovie", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers:{'content-type':'application/json'},
-    }).then((response) => response);
-  } 
-  catch (error) {
-    alert("sorry about the error");
-  } 
-  finally {
-  }
-}
-
+//   getMovieByName().then((result)=>{
+//     result.data.forEach(movieItem=>
+//       div_id.innerHTML +=`<li>${movieItem.}</li>`
+//       )
+//   })
+// }
